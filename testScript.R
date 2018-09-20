da2a1cb5e0208bdb17c1dfbfb408ad64b02d31ee
JasonKChow <jason.k.chow@vanderbilt.edu>
2018-09-20 14:15:24 -0500

    test keras script

---
+++ testScript.R
@@ -0,0 +1,55 @@
+#### Load environment ####
+library(keras)
+
+# Read iris dataset
+iris <- read.csv(
+  url("http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"),
+  header = FALSE)
+
+#### Data preparation ####
+# Determine sample size
+ind <- sample(2, nrow(iris), replace = TRUE, prob = c(0.67, 0.33))
+
+# Split the `iris` data
+iris.training <- iris[ind == 1, 1:4]
+iris.test <- iris[ind == 2, 1:4]
+
+# Split the class attribute
+iris.trainingtarget <- iris[ind == 1, 5]
+iris.testtarget <- iris[ind == 2, 5]
+
+# One hot encode training target values
+iris.trainLabels <- to_categorical(iris.trainingtarget)
+
+# One hot encode test target values
+iris.testLabels <- to_categorical(iris.testtarget)
+
+#### Create model ####
+# Initialize a sequential model
+model <- keras_model_sequential()
+
+# Add layers to the model
+model %>%
+  layer_dense(units = 8, activation = 'relu', input_shape = c(4)) %>%
+  layer_dense(units = 3, activation = 'softmax')
+
+# Compile the model
+model %>% compile(
+  loss = 'categorical_crossentropy',
+  optimizer = 'adam',
+  metrics = 'accuracy'
+)
+
+#### Fit model ####
+# Fit the model
+# Store the fitting history in `history`
+history <- model %>% fit(
+  iris.training,
+  iris.trainLabels,
+  epochs = 200,
+  batch_size = 5,
+  validation_split = 0.2
+)
+
+#### Outputs ####
+save_model_hdf5(model, 'test_mode.h5')
